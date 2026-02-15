fe0d1cf746aec2296f302f2e9814a697e9c0fab5
Leena IKHLEF <leena.ikhlef@ensia.edu.dz>
2026-02-15 17:32:37 +0100

    add technical card to home page

--- ebec-admin/client/src/pages/Home.jsx
+++ ebec-admin/client/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+﻿import React, { useState, useEffect, useRef } from 'react';
 import ebecLogo from '../assets/EBEC.jfif';
 
 // --- Custom SVG Icons ---
